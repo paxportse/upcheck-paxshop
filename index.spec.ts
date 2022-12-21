d0ff6d7e603ae89da6d4e94cd94b3cbc04338c27
Benjamin Josefsson <benjamin.josefsson@paxport.net>
2022-12-21 10:31:43 +0100

    Setup testing.

--- index.spec.ts
+++ index.spec.ts
@@ -1,8 +1,17 @@
-import "jest"
-import * as library from "./index"
+import "isomorphic-fetch"
+import * as model from "@paxport/model-paxshop"
+import * as dotenv from "dotenv"
 
-describe("library", () => {
-	it("a", () => {
-		expect(library.a).toEqual(42)
+dotenv.config()
+
+describe("model.Client", () => {
+	it("booking.fetch", async () => {
+		const client = model.Client.create(
+			process.env.apiUrl || "https://api.shop.paxport.group",
+			"https://test.shop.paxport.group"
+		)
+		expect(await client.booking.fetch({ reference: "AAAA12", departure: "2022-01-13" })).toMatchSnapshot(
+			"booking-AAAA12"
+		)
 	})
 })
